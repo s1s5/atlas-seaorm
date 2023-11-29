6002c2a91322aac874559d8ea875dfbc9bafe0e6
Shogo Sawai <shogo.sawai@gmail.com>
2023-11-29 17:36:58 +0900

    wip

--- atlasutil.py
+++ atlasutil.py
@@ -1,18 +1,41 @@
 import datetime
 import glob
+import logging
 import os
 import random
 import re
 import select
 import shutil
 import subprocess
+import sys
 import tempfile
 import time
+import typing
 import uuid
 from subprocess import run
 
+logger = logging.getLogger(__name__)
+
+
+def setup_log(log_level: typing.Literal["debug", "info", "error"]):
+    """loggerのセットアップ"""
+    handler = logging.StreamHandler(sys.stderr)
+    handler.setFormatter(
+        logging.Formatter("%(asctime)s [%(levelname)s] %(name)s:%(lineno)d %(message)s")
+    )
+    logger.addHandler(handler)
+
+    logger.setLevel(
+        {
+            "debug": logging.DEBUG,
+            "info": logging.INFO,
+            "error": logging.ERROR,
+        }[log_level.lower()]
+    )
+
 
 def get_previous_schema(td: str, down_dir: str):
+    """前回のマイグレーション後のスキーマ"""
     if os.path.exists(down_dir):
         return sorted(glob.glob(os.path.join(down_dir, "schema-*")), reverse=True)[0]
     previous_schema = os.path.join(td, uuid.uuid4().hex + ".sql")
@@ -22,10 +45,12 @@ def get_previous_schema(td: str, down_dir: str):
 
 
 def get_latest_migration_file(dir: str):
+    """最新のマイグレーションファイル"""
     return sorted(glob.glob(os.path.join(dir, "*.sql")), reverse=True)[0]
 
 
 def copy_schema(to: str, name: str, down_dir: str):
+    """down migrationを作成するために、スキーマをコピー"""
     if os.path.isfile(to):
         shutil.copy(to, os.path.join(down_dir, f"schema-{name}"))
     else:
@@ -33,17 +58,21 @@ def copy_schema(to: str, name: str, down_dir: str):
 
 
 def make_migration(dir: str, name: str, to: str):
+    """schemaマイグレーションのsqlファイル作成"""
     td = tempfile.mkdtemp()
     up_dir = os.path.join(dir, "up")
     down_dir = os.path.join(dir, "down")
 
     previous_schema = get_previous_schema(td=td, down_dir=down_dir)
+    logger.info("previous schema = %s", previous_schema)
 
     try:
         prev_migration_file = get_latest_migration_file(up_dir)
     except IndexError:
         prev_migration_file = None
+    logger.info("previous migration file = %s", prev_migration_file)
 
+    logger.info("creating up migration file")
     run(
         [
             "atlas",
@@ -63,11 +92,14 @@ def make_migration(dir: str, name: str, to: str):
     )
     up_migration_file = get_latest_migration_file(up_dir)
     if prev_migration_file == up_migration_file:
+        logger.info("no migration created")
         return
+    logger.info("new migration file = %s", up_migration_file)
 
     try:
         temp_up_dir = os.path.join(td, "up")
         shutil.copytree(up_dir, temp_up_dir)
+        logger.info("creating down migration file")
         run(
             [
                 "atlas",
@@ -86,12 +118,14 @@ def make_migration(dir: str, name: str, to: str):
             check=True,
         )
         down_migration_file = get_latest_migration_file(temp_up_dir)
+        logger.info("new 'down' migration file =%s", down_migration_file)
         os.makedirs(down_dir, exist_ok=True)
         shutil.copy(
             down_migration_file,
             os.path.join(down_dir, os.path.basename(up_migration_file)),
         )
 
+        logger.info("coping current schema")
         copy_schema(
             to=to,
             name=os.path.basename(up_migration_file),
@@ -136,8 +170,10 @@ impl MigrationTrait for Migration {
 
 
 def generate_seaorm_migration(name: str, src_dir: str, dir: str, basename: str):
+    """sea-ormのmigrationファイルの生成"""
     t = os.path.splitext(basename)[0]
     mod_name = "m" + t[:8] + "_" + t[8:]  # seaormのスタイルに合わせる
+    logger.info("create seaorm migration file %s", mod_name)
     up_fn = os.path.relpath(os.path.join(dir, "up", basename), src_dir)
     down_fn = os.path.relpath(os.path.join(dir, "down", basename), src_dir)
     up_cmd = EXEC_SQL % up_fn
@@ -150,6 +186,9 @@ def generate_seaorm_migration(name: str, src_dir: str, dir: str, basename: str):
 
 
 def update_seaorm_lib(src_dir: str, mod_name: str):
+    """lib.rsにmoduleを追加して、migrationsにも追加する"""
+    logger.info("update %s/lib.rs for %s", src_dir, mod_name)
+
     with open(os.path.join(src_dir, "lib.rs"), "r") as fp:
         lines = fp.read().splitlines()
 
@@ -180,33 +219,26 @@ def update_seaorm_lib(src_dir: str, mod_name: str):
     run(["cargo", "fmt", "--", os.path.join(src_dir, "lib.rs")], check=True)
 
 
-def create_schema_migration(
-    dir: str, name: str, to: str, src_dir: str, **_other_kwargs
-):
-    migration_filename = make_migration(dir=dir, name=name, to=to)
-    if migration_filename is None:
-        return
-    mod_name = generate_seaorm_migration(
-        name=name, src_dir=src_dir, dir=dir, basename=migration_filename
-    )
-    update_seaorm_lib(src_dir=src_dir, mod_name=mod_name)
+def add_mod_to_lib(src_dir: str, mod_name: str):
+    """lib.rsにimportするmoduleを追加する"""
+    logger.info("add %s to %s/lib.rs ", mod_name, src_dir)
 
+    with open(os.path.join(src_dir, "lib.rs"), "r") as fp:
+        lines = fp.read().splitlines()
 
-def create_data_migration(name: str, src_dir: str, dir: str, **_other_kwargs):
-    now = datetime.datetime.utcnow()
-    mod_name = now.strftime(f"m%Y%m%d_%H%M%S_{name}")
+    xp = re.compile(r"mod\s+m[a-zA-Z0-9_]+;")
+    line_index = [x[0] for x in enumerate(lines) if xp.match(x[1])][-1]
+    lines.insert(line_index + 1, f"mod {mod_name};")
 
-    schema = sorted(
-        glob.glob(os.path.join(os.path.join(dir, "down"), "schema-*")), reverse=True
-    )[0]
-    generate_entity(to=schema, src_dir=src_dir, mod_name=mod_name)
-    with open(os.path.join(src_dir, mod_name, "mod.rs"), "w") as fp:
-        fp.write(MIGRATION_FILE_TEMPLATE % ("mod entity;", "", ""))
+    with open(os.path.join(src_dir, "lib.rs"), "w") as fp:
+        fp.write("\n".join(lines))
 
-    update_seaorm_lib(src_dir=src_dir, mod_name=mod_name)
+    run(["cargo", "fmt", "--", os.path.join(src_dir, "lib.rs")], check=True)
 
 
 def run_postgres(port: int):
+    """postgresをdocker内で走らせる"""
+    logger.info("running postgres in docker")
     p = subprocess.Popen(
         [
             "docker",
@@ -235,17 +267,20 @@ def run_postgres(port: int):
                     break
             if time.time() > timeout or i >= 999:
                 raise Exception("timeout")
+        logger.info("postgres started")
         return p
     except Exception:
         p.kill()
         raise
 
 
-def generate_entity(to: str, src_dir: str, mod_name: str):
+def generate_entity(to: str, ent_dir: str):
+    """entityファイルを生成する"""
     port = random.randint(10000, 65536)
     p = run_postgres(port)
     try:
         postgres_url = f"postgres://postgres:pass@localhost:{port}/postgres"
+        logger.info("applying schema %s", to)
         run(
             [
                 "atlas",
@@ -262,6 +297,7 @@ def generate_entity(to: str, src_dir: str, mod_name: str):
             check=True,
             capture_output=True,
         )
+        logger.info("creating current entity to %s", ent_dir)
         run(
             [
                 "sea-orm-cli",
@@ -270,7 +306,7 @@ def generate_entity(to: str, src_dir: str, mod_name: str):
                 "-u",
                 postgres_url,
                 "-o",
-                os.path.join(src_dir, mod_name, "entity"),
+                ent_dir,
             ],
             check=True,
             capture_output=True,
@@ -279,15 +315,53 @@ def generate_entity(to: str, src_dir: str, mod_name: str):
         p.terminate()
 
 
+def create_schema_migration(
+    dir: str, name: str, to: str, src_dir: str, **_other_kwargs
+):
+    migration_filename = make_migration(dir=dir, name=name, to=to)
+    if migration_filename is None:
+        return
+    mod_name = generate_seaorm_migration(
+        name=name, src_dir=src_dir, dir=dir, basename=migration_filename
+    )
+    update_seaorm_lib(src_dir=src_dir, mod_name=mod_name)
+
+
+def create_data_migration(name: str, src_dir: str, dir: str, **_other_kwargs):
+    now = datetime.datetime.utcnow()
+    mod_name = now.strftime(f"m%Y%m%d_%H%M%S_{name}")
+
+    schema = sorted(
+        glob.glob(os.path.join(os.path.join(dir, "down"), "schema-*")), reverse=True
+    )[0]
+    generate_entity(to=schema, ent_dir=os.path.join(src_dir, f"{mod_name}_entity"))
+    with open(os.path.join(src_dir, f"{mod_name}.rs"), "w") as fp:
+        fp.write(
+            MIGRATION_FILE_TEMPLATE
+            % (f"use super::{mod_name}_entity as entity;", "", "")
+        )
+
+    update_seaorm_lib(src_dir=src_dir, mod_name=mod_name)
+    add_mod_to_lib(src_dir=src_dir, mod_name=f"{mod_name}_entity")
+
+
+def create_entity_files(dir: str, ent_dir: str, **_other_kwargs):
+    migration_dir = os.path.join(dir, "up")
+    generate_entity(to=migration_dir, ent_dir=ent_dir)
+
+
 def __entry_point():
     import argparse
 
     parser = argparse.ArgumentParser(
         description="create migration files",  # プログラムの説明
     )
+    parser.add_argument("--log-level", default="info")
     subparsers = parser.add_subparsers()
 
-    parser_schema = subparsers.add_parser("schema", help="see `schema -h`")
+    parser_schema = subparsers.add_parser(
+        "schema", help="create schema migration files"
+    )
     parser_schema.add_argument("--name", default="schema")
     parser_schema.add_argument("--to", default="migration/schema")
     parser_schema.add_argument("--dir", default="migration/atlas")
@@ -296,7 +370,7 @@ def __entry_point():
         handler=lambda args: create_schema_migration(**dict(args._get_kwargs()))
     )
 
-    parser_data = subparsers.add_parser("data", help="see `data -h`")
+    parser_data = subparsers.add_parser("data", help="create data migration files")
     parser_data.add_argument("--name", default="data")
     parser_data.add_argument("--dir", default="migration/atlas")
     parser_data.add_argument("--src-dir", default="migration/src")
@@ -304,7 +378,16 @@ def __entry_point():
         handler=lambda args: create_data_migration(**dict(args._get_kwargs()))
     )
 
+    parser_data = subparsers.add_parser("entity", help="create entity files")
+    parser_data.add_argument("--dir", default="migration/atlas")
+    parser_data.add_argument("--ent-dir", default="src/entity")
+    parser_data.set_defaults(
+        handler=lambda args: create_entity_files(**dict(args._get_kwargs()))
+    )
+
     args = parser.parse_args()
+    setup_log(args.log_level)
+
     if hasattr(args, "handler"):
         args.handler(args)
     else:
