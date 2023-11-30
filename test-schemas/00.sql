a70739cfb34d1d11649be03c0408646c45a5b855
Shogo Sawai <shogo.sawai@gmail.com>
2023-11-30 13:08:25 +0900

    format

--- test-schemas/00.sql
+++ test-schemas/00.sql
@@ -1,1 +1,5 @@
-CREATE TABLE "test_user" ( "id" bigserial NOT NULL PRIMARY KEY, "name" varchar NOT NULL )
+CREATE TABLE
+    "test_user" (
+        "id" bigserial NOT NULL PRIMARY KEY,
+        "name" varchar NOT NULL
+    )
--- test-schemas/01.sql
+++ test-schemas/01.sql
@@ -1,2 +1,11 @@
-CREATE TABLE "test_manager" ( "id" bigserial NOT NULL PRIMARY KEY, "name" varchar NOT NULL );
-CREATE TABLE "test_user" ( "id" bigserial NOT NULL PRIMARY KEY, "name" varchar NOT NULL );
+CREATE TABLE
+    "test_manager" (
+        "id" bigserial NOT NULL PRIMARY KEY,
+        "name" varchar NOT NULL
+    );
+
+CREATE TABLE
+    "test_user" (
+        "id" bigserial NOT NULL PRIMARY KEY,
+        "name" varchar NOT NULL
+    );
